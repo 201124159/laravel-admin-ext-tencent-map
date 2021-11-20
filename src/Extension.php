fbb4cc0a2bc3992805f8d6201e773e3a34a827ba
json <201124159@qq.com>
2021-11-20 21:14:48 +0800

    init

---
+++ src/Extension.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tutu\LaravelAdminTencentMap;
+
+use Encore\Admin\Extension as BaseExtension;
+
+class Extension extends BaseExtension
+{
+    public $name = 'tentcent-map';
+
+    public $views = __DIR__.'/../resources/views';
+}
